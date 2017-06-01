b990512ac2b3058d6f91d9ca13f1c2880600cb99
rsazor12 <bielskikamil@gmail.com>
2017-06-01 19:51:39 +0200

    Działający w pełni algorytm genetyczny - penalty 20

---
+++ NURSESCHEDULING_FINAL_PROJECT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Constraints.Tests")]
+
+
+namespace NURSESCHEDULING_FINAL_PROJECT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.SetBufferSize(200, 100);
+            ChromosomeClass obChromosomeClass = new ChromosomeClass();
+            //obChromosomeClass.writeNursesFromChromosomeFromEachShift();
+
+
+
+            GeneticAlgorithmClass obGeneticAlgorithmClass = new GeneticAlgorithmClass(6,10,10,1000,1000,4);
+            obChromosomeClass = obGeneticAlgorithmClass.runAlgorithm();  //uruchom lagorytm , a gdy wsytskie constraints spełnione zwróć najlepszy chromosom
+
+            obChromosomeClass.writeNursesFromChromosomeFromEachShift();  //wypisz ten chromosom ktory jest wynikiem
+
+             Console.WriteLine("\n\nPenalty rozwiązania wynosi " + obChromosomeClass.PenaltyOfChromosome);
+
+           
+
+
+
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
